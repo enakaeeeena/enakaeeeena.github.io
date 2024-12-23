6f775ff6d64e50360f76d21dbd675967d7cc51cb
enakaeeeena <166594070+enakaeeeena@users.noreply.github.com>
2024-12-23 13:17:09 +0300

    2

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-function scrollToSection(sectionId) {
+// function scrollToSection(sectionId) {
+//     const section = document.getElementById(sectionId);
+//     if (section) {
+//         section.scrollIntoView({ behavior: 'smooth' });
+//     }
+// }
+function scrollToSection(event, sectionId) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
     const section = document.getElementById(sectionId);
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
 }
-
 function sendEmail() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    if (name && email && message) {
-        // Здесь вы можете использовать API для отправки email
-        // Например, используя fetch для отправки данных на сервер
-
-        fetch('https://api.mail.ru/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                to: 'enakaena@mail.ru',
-                subject: 'Новое сообщение от ' + name,
-                text: `Имя: ${name}\nEmail: ${email}\nСообщение: ${message}`
-            })
-        })
-        .then(response => {
-            if (response.ok) {
-                alert('Сообщение отправлено!');
-                document.getElementById('feedback-form').reset(); // Сброс формы
-            } else {
-                alert('Ошибка при отправке сообщения.');
-            }
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-            alert('Ошибка при отправке сообщения.');
-        });
-    } else {
+    if (!name || !email || !message) {
         alert('Пожалуйста, заполните все поля.');
+        return;
     }
+
+    const subject = encodeURIComponent('Сообщение от ' + name);
+    const body = encodeURIComponent('Имя: ' + name + '\nПочта: ' + email + '\nСообщение: ' + message);
+    
+    const mailtoLink = `mailto:your-email@example.com?subject=${subject}&body=${body}`;
+    
+    window.location.href = mailtoLink;
 }
